578a73c447a1d5445cade74758b32ba2cae718fd
shubhraneel <shubhraneelpal@gmail.com>
2020-05-05 08:14:45 +0530

    Added start and restart button

--- index.js
+++ index.js
@@ -5,43 +5,64 @@ let canvasColor = "#0c7b93";
 let appleColor = "#d11c1c";
 let height = 500;
 let width = 1000;
+let unit = 20;
 
 let canvas = document.getElementById("game-canvas");
 let h = height;
 let w = width;
 let canv = canvas.getContext("2d");
-let dir = { x: 1, y: 0 };
-
-canv.fillStyle = canvasColor;
-canv.fillRect(0, 0, w, h);
-
-let unit = 20;
+let gameOver = document.querySelector(".game-end");
 
 const rand = (x) => Math.floor((Math.random() * x) / unit) * unit;
 
-let snake = [{ x: rand(w), y: rand(h) }];
-
-let apple = {x: rand(w), y: rand(h)};
-
-let appleEaten = false;
-
-let score = 0;
-
+let dir;
+let snake;
+let apple;
+let appleEaten;
+let score;
 let scoreVal = document.querySelector(".score--value");
-scoreVal.textContent = score;
 
-const turn = event => {
+const initialize = () => {
+  dir = { x: 1, y: 0 };
+  canv.fillStyle = canvasColor;
+  canv.fillRect(0, 0, w, h);
+
+  unit = 20;
+
+  snake = [{ x: rand(w), y: rand(h) }];
+
+  apple = { x: rand(w), y: rand(h) };
+
+  appleEaten = false;
+
+  score = 0;
+  scoreVal.textContent = score;
+};
+
+const turn = (event) => {
   const key = event.key;
-  if ((key === "w" || key === "ArrowUp" || key === "8" || key === "h") && dir.y === 0) {
+  if (
+    (key === "w" || key === "ArrowUp" || key === "8" || key === "h") &&
+    dir.y === 0
+  ) {
     dir.x = 0;
     dir.y = -1;
-  } else if ((key === "a" || key === "ArrowLeft" || key === "4" || key === "j") && dir.x === 0) {
+  } else if (
+    (key === "a" || key === "ArrowLeft" || key === "4" || key === "j") &&
+    dir.x === 0
+  ) {
     dir.x = -1;
     dir.y = 0;
-  } else if ((key === "s" || key === "ArrowDown" || key === "2" || key === "k") && dir.y === 0) {
+  } else if (
+    (key === "s" || key === "ArrowDown" || key === "2" || key === "k") &&
+    dir.y === 0
+  ) {
     dir.x = 0;
     dir.y = 1;
-  } else if ((key === "d" || key === "ArrowRight" || key === "6" || key === "l") && dir.x === 0) {
+  } else if (
+    (key === "d" || key === "ArrowRight" || key === "6" || key === "l") &&
+    dir.x === 0
+  ) {
     dir.x = 1;
     dir.y = 0;
   }
@@ -50,93 +71,96 @@ const turn = event => {
 document.addEventListener("keydown", turn);
 
 const enqueue = (xdir, ydir) => {
-  snake.unshift({x: snake[0].x + xdir*unit, y: snake[0].y + ydir*unit});
-  if(snake[0].x >= w) snake[0].x = 0;
-  else if(snake[0].x < 0) snake[0].x = w - unit;
-  if(snake[0].y >= h) snake[0].y = 0;
-  else if(snake[0].y < 0) snake[0].y = h - unit;
-  if(snake[0].x === apple.x && snake[0].y === apple.y)
-    appleEaten = true;
+  snake.unshift({ x: snake[0].x + xdir * unit, y: snake[0].y + ydir * unit });
+  if (snake[0].x >= w) snake[0].x = 0;
+  else if (snake[0].x < 0) snake[0].x = w - unit;
+  if (snake[0].y >= h) snake[0].y = 0;
+  else if (snake[0].y < 0) snake[0].y = h - unit;
+  if (snake[0].x === apple.x && snake[0].y === apple.y) appleEaten = true;
 };
 
 const drawApple = (apple) => {
-  canv.fillStyle = 'orangered';
+  canv.fillStyle = "orangered";
   canv.fillRect(apple.x, apple.y, unit, unit);
 };
-
-drawApple(apple);
 
 const clearCanvas = () => {
   canv.fillStyle = canvasColor;
 
   canv.fillRect(0, 0, w, h);
-}
+};
 
 const generateApple = () => {
-  while(true) {
+  while (true) {
     appleOnSnake = false;
-    apple = {x: rand(w), y: rand(h)};
-    snake.forEach(element => {
-      if(apple.x === element.x && apple.y === element.y) {
+    apple = { x: rand(w), y: rand(h) };
+    snake.forEach((element) => {
+      if (apple.x === element.x && apple.y === element.y) {
         appleOnSnake = true;
       }
-    })
-    if(appleOnSnake === false)
-      return apple;
+    });
+    if (appleOnSnake === false) return apple;
   }
-}
+};
 
 const move = () => {
   enqueue(dir.x, dir.y);
-  if(appleEaten) {
+  if (appleEaten) {
     apple = generateApple();
     appleEaten = false;
-    score+=10;
+    score += 10;
     scoreVal.textContent = score;
-  } 
-  else {
+  } else {
     snake.pop();
   }
 };
 
 const isDead = () => {
-  for(let i=1; i < snake.length; i++)
-    if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) 
-      return true;
+  for (let i = 1; i < snake.length; i++)
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) return true;
   return false;
 };
 
 const draw = () => {
   canv.fillStyle = snakeColor;
   canv.strokeStyle = "black";
-  snake.forEach(element => {
+  snake.forEach((element) => {
     canv.fillRect(element.x, element.y, unit, unit);
     canv.strokeRect(element.x, element.y, unit, unit);
   });
   canv.fillStyle = snakeHead;
   canv.fillRect(snake[0].x, snake[0].y, unit, unit);
-
 };
 
-const step = t1 => t2 => {
-  if(isDead()) {
+const gameEnd = () => {
+  gameOver.style.display = "block";
+};
+
+document.querySelector(".play-again").addEventListener("click", () => {
+  gameOver.style.display = "none";
+  document.querySelector(".game-over").style.visibility = "visible";
+  initialize();
+  requestAnimationFrame(step(0));
+});
+
+const step = (t1) => (t2) => {
+  if (isDead()) {
     gameEnd();
     return;
   }
 
-  if(t2-t1>gameSpeed) {
+  if (t2 - t1 > gameSpeed) {
     move();
     clearCanvas();
     drawApple(apple);
     draw();
     window.requestAnimationFrame(step(t2));
-  }
-
-  else {
+  } else {
     window.requestAnimationFrame(step(t1));
   }
-}
+};
 
-
-
-requestAnimationFrame(step(0));
+gameOver.style.display = "block";
+document.querySelector(".game-over").style.visibility = "hidden";
+document.querySelector(".play-again").textContent = "Start game";
+clearCanvas();
